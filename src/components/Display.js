6a5eb424ec21b6f62a610473b58820c420508940
soeon Kim <developeon80@gmail.com>
2018-10-28 22:39:08 +0900

    Enter 페이지의 css 파일 분리

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Display.css';
 
 function Display({phone}){
   return (
-    <div className="Enter-display">
-      <p className="Enter-input">{phone}</p>
-      <p className="Enter-terms">이용약관과 개인정보 취급방침에 동의하시면 휴대전화번호 입력 후 아래 적립 버튼을 눌러주세요.</p>
+    <div className="Display-display">
+      <p className="Display-input">{phone}</p>
+      <p className="Display-terms">이용약관과 개인정보 취급방침에 동의하시면 휴대전화번호 입력 후 아래 적립 버튼을 눌러주세요.</p>
     </div>
   );
 }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Keypad.css';
 
 class Keypad extends Component {
     handleSubmit = () => {
@@ -16,25 +17,25 @@ class Keypad extends Component {
     render() {
         return (
             <>
-            <div className="Enter-keypad">
+                <div className="Keypad-group">
                     <button onClick={this.handleClick} value="1">1</button>
                     <button onClick={this.handleClick} value="2">2</button>
                     <button onClick={this.handleClick} value="3">3</button>
                 </div>
-                <div className="Enter-keypad">
+                <div className="Keypad-group">
                     <button onClick={this.handleClick} value="4">4</button>
                     <button onClick={this.handleClick} value="5">5</button>
                     <button onClick={this.handleClick} value="6">6</button>
                 </div>
-                <div className="Enter-keypad">
+                <div className="Keypad-group">
                     <button onClick={this.handleClick} value="7">7</button>
                     <button onClick={this.handleClick} value="8">8</button>
                     <button onClick={this.handleClick} value="9">9</button>
                 </div>
-                <div className="Enter-keypad">
+                <div className="Keypad-group">
                     <button onClick={this.handleRemove}>&larr;</button>
                     <button onClick={this.handleClick} value="0">0</button>
-                    <button id="Enter-button" onClick={this.handleSubmit}>적립</button>
+                    <button id="Keypad-button" onClick={this.handleSubmit}>적립</button>
                 </div>
             </>
         );
