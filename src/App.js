8c5536a7a7830f47c551ca978c004728e1344335
soeon Kim <developeon80@gmail.com>
2018-10-28 23:38:29 +0900

    폴더명 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './container/Home';
-import Enter from './container/Enter';
-import Result from './container/Result';
-import NoMatch from './container/NoMatch';
+import Home from './pages/Home';
+import Enter from './pages/Enter';
+import Result from './pages/Result';
+import NoMatch from './pages/NoMatch';
 
 class App extends Component {
   render() {
